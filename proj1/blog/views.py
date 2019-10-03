8046ad4b23b0794bad8cf2ea68a5091c24597ddb
JsilverPi <fivisitw@gmail.com>
2019-10-03 23:07:40 +0900

    20191003-23:07

--- proj1/blog/views.py
+++ proj1/blog/views.py
@@ -13,12 +13,11 @@ from  django.db.models import F
 def post_list(request):
 #
 #
-#  posts=Post_M.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+   posts_a=Post_M.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 #  post_last=Post_M.objects.order_by('published_date').last()
    posts=Post_M.objects.order_by(F('published_date').desc(nulls_last=True)).all()[:3]
 #  return  render(request,'blog/post_list.html',{'posts': posts, 'post_last':post_last})
-   return  render(request,'blog/post_list.html',{'posts': posts })
-
+   return  render(request,'blog/post_list.html',{'posts': posts , 'posts_a':posts_a})
 def post_detail(request,pk):
   post=get_object_or_404(Post_M,pk=pk)
   return  render(request,'blog/post_detail.html',{'post': post})
